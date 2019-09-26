7433950f95890775460a30b079ce7aeb593af0c1
Rezky Nur Fauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-09-26 12:40:28 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan9-Teks/src/teks/Teks.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teks;
+
+/**
+ *
+ * Nama : Rezky Nur Fauzi
+ * NIM  : 10118016  
+ * Kelas: IF-1 
+ * Deskripsi Program : Penggabungan Teks pada Java
+ */
+public class Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        
+        System.out.println("Hasil" + newline + "Char :" + tab + "berisi " + c);
+    }
+    
+}
